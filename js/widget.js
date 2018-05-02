96a6230d939c195892cd71e2e79cf8e10ad03d9b
Natacha Romano <33462970+romanonatacha@users.noreply.github.com>
2018-05-02 20:30:14 -0300

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -1,42 +1,30 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4) {
-    var employees = JSON.parse(xhr.responseText);
+$(document).ready(function () {
+  $.getJSON('../data/employees.json', function (data) {
     var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
       } else {
-        statusHTML += '<li class="out">';
+        statusHTML +='<li class="out">';
       }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
-    }
+      statusHTML += employee.name + '</li>';
+    });
     statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
-xhr.open('GET', '../data/employees.json');
-xhr.send();
-
-var roomRequest = new XMLHttpRequest();
-roomRequest.onreadystatechange = function () {
-  if(roomRequest.readyState === 4) {
-    var rooms = JSON.parse(roomRequest.responseText);
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+  
+  $.getJSON('../data/rooms.json', function (data) {
     var statusHTML = '<ul class="rooms">';
-    for (var i=0; i<rooms.length; i += 1) {
-      if (rooms[i].available === true) {
-        statusHTML += '<li class="empty">';
+    $.each(data,function (index, rooms) {
+      if (rooms.available === true) {
+        statusHTML +='<li class="empty">';
       } else {
-        statusHTML += '<li class="full">';
+        statusHTML +='<li class="full">';
       }
-      statusHTML += rooms[i].room;
-      statusHTML += '</li>';
-    }
+      statusHTML += rooms.room + '</li>';
+    });
     statusHTML += '</ul>';
-    document.getElementById('roomList').innerHTML = statusHTML;
-  }
-};
-roomRequest.open('GET', '../data/rooms.json');
-roomRequest.send();
-
+    $('#roomList').html(statusHTML)
+    });
+  
+}); // end ready
